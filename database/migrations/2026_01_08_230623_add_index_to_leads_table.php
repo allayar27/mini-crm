34cc76959f90d14f1776fcc9787bc04d8829a768
allayar27 <galaxyasamsung979@gmail.com>
2026-01-08 23:11:02 +0500

    added index

---
+++ database/migrations/2026_01_08_230623_add_index_to_leads_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            $table->index('assigned_to');
+            $table->index('status');
+            $table->index('created_at');
+            $table->unique('phone');
+            $table->index(['assigned_to', 'status', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('leads', function (Blueprint $table) {
+            //
+        });
+    }
+};
